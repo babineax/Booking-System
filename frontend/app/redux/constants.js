fe1e4aabe3ee0b6d793cc8001d5505d0aebd985d
gideon <kipkerring.gedion@gmail.com>
2025-08-05 18:49:15 +0300

    constants

--- frontend/app/redux/constants.js
+++ frontend/app/redux/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const SERVICES_URL = "/api/services";
-export const BOOKINGS_URL = "/api/bookings";
-export const BUSINESS_HOURS_URL = "/api/business-hours";
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const SERVICES_URL = `${BASE_URL}/api/services`;
+export const BOOKINGS_URL = `${BASE_URL}/api/bookings`;
+export const BUSINESS_HOURS_URL = `${BASE_URL}/api/business-hours`;

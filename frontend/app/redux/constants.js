7c37277cdd344877046949a543bdcd0dc57405a8
gideon <kipkerring.gedion@gmail.com>
2025-08-05 18:08:02 +0300

    bussinessHoursApiSlice

--- frontend/app/redux/constants.js
+++ frontend/app/redux/constants.js
@@ -2,4 +2,4 @@ export const BASE_URL = "";
 export const USERS_URL = "/api/users";
 export const SERVICES_URL = "/api/services";
 export const BOOKINGS_URL = "/api/bookings";
-export const BUSINESSHOURS_URL = "/api/business-hours";
+export const BUSINESS_HOURS_URL = "/api/business-hours";

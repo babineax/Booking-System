49768450c3de318999077fd739cfd1f28bc03fa3
gideon <kipkerring.gedion@gmail.com>
2025-08-06 14:08:56 +0300

    testing

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,17 +1,11 @@
-import { Stack } from "expo-router";
-import "./globals.css"; // Import global styles
-export default function RootLayout() {
-  return <Stack />;
-}
-// app/_layout.tsx or app/_app.tsx (depends on your setup)
-// import { Slot } from "expo-router"; // or your routing mechanism
-// import { Provider } from "react-redux";
-// import { store } from "../app/redux/store"; // adjust path as needed
+import { Stack } from "expo-router"; // or your routing mechanism
+import { Provider } from "react-redux";
+import { store } from "../app/redux/store"; // adjust path as needed
 
-// export default function RootLayout() {
-//   return (
-//     <Provider store={store}>
-//       <Slot /> {/* This renders nested screens like RegisterScreen */}
-//     </Provider>
-//   );
-// }
+export default function RootLayout() {
+  return (
+    <Provider store={store}>
+      <Stack /> {/* This renders nested screens like RegisterScreen */}
+    </Provider>
+  );
+}

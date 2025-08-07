af998edb27496ba37dd031ae8be804a406ebcf03
gideon <kipkerring.gedion@gmail.com>
2025-08-07 18:34:42 +0300

    getServiceDetailsById

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,11 +1,15 @@
 import { Stack } from "expo-router";
+import Toast from "react-native-toast-message";
 import { Provider } from "react-redux";
 import { store } from "../src/redux/store";
 
 export default function RootLayout() {
   return (
     <Provider store={store}>
-      <Stack /> {/* This renders nested screens like RegisterScreen */}
+      <>
+        <Toast position="top" visibilityTime={3000} />
+        <Stack /> {/* This renders nested screens like RegisterScreen */}
+      </>
     </Provider>
   );
 }

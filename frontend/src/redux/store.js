5592fb1c4909b82ad495f48ca70e6a17748bd2c9
nigeltheyounger <nijomarungu@gmail.com>
2025-08-20 22:21:08 +0300

    git test

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,6 +4,7 @@ import { apiSlice } from "./apis/apiSlice";
 import authReducer from "./features/auth/authSlice";
 
 export const store = configureStore({
+  
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,

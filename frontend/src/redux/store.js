80fc53e73fae504fe6163bfd95a5dba1b309b981
gideon <kipkerring.gedion@gmail.com>
2025-09-01 11:59:09 +0300

    refactor(redux): update store to use only Firebase slices
    - Removed reducers and middleware for deleted REST API slices
    - Store now only includes Firebase-based slices and auth

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
-import { apiSlice } from "./apis/apiSlice";
 import { firebaseBookingsApiSlice } from "./apis/firebaseBookingsApiSlice";
 import { firebaseBusinessHoursApiSlice } from "./apis/firebaseBusinessHoursApiSlice";
 import { firebaseServicesApiSlice } from "./apis/firebaseServicesApiSlice";
@@ -9,29 +8,26 @@ import authReducer from "./features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
-      
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    
     [firebaseUsersApiSlice.reducerPath]: firebaseUsersApiSlice.reducer,
     [firebaseServicesApiSlice.reducerPath]: firebaseServicesApiSlice.reducer,
     [firebaseBookingsApiSlice.reducerPath]: firebaseBookingsApiSlice.reducer,
-    [firebaseBusinessHoursApiSlice.reducerPath]: firebaseBusinessHoursApiSlice.reducer,
+    [firebaseBusinessHoursApiSlice.reducerPath]:
+      firebaseBusinessHoursApiSlice.reducer,
     auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-        ignoredPaths: ['firebase', 'firestore'],
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+        ignoredPaths: ["firebase", "firestore"],
       },
     }).concat(
-      apiSlice.middleware,
       firebaseUsersApiSlice.middleware,
       firebaseServicesApiSlice.middleware,
       firebaseBookingsApiSlice.middleware,
       firebaseBusinessHoursApiSlice.middleware
     ),
-  devTools: true,
+  devTools: "production",
 });
 
 setupListeners(store.dispatch);
